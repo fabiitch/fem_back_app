03f51bd8f0e85694667eb50152994e99ea1ebc89
fabiitch <fabien.occelli@hotmail.fr>
2020-05-10 02:57:39 +0200

    android X

--- android/app/src/main/java/com/fem/fem_back_app/MainActivity.java
+++ android/app/src/main/java/com/fem/fem_back_app/MainActivity.java
@@ -1,6 +1,6 @@
 package com.fem.fem_back_app;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
